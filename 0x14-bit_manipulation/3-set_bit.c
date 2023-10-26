507ad8c6a2181c1199628016e8ed6fcb27b15370
kiychu1225 <aklildereje72@gmail.com>
2023-10-26 22:12:13 +0300

    i did it

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_bit -  find the value of a bit at a given index.
+ * @n: decimal number
+ * @index: index starting from 0 of the bit you want to get
+ *
+ * Return: value of a bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32) /* because size of long int is 32 bits */
+		return (-1);
+
